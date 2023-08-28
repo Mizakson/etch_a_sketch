ab6aa4b9d96903e53d7870342d9c572b87eaee30
Mizakson <maxwellizakson@gmail.com>
2023-08-28 11:52:57 -0400

    Add createGrid function to .js file

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+
+function createGrid(amtOfGrids) {
+    for (let i = 0; i < amtOfGrids; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+        for (let j = 0; j < amtOfGrids; j++) {
+            const gridBox = document.createElement('div');
+            gridBox.classList.add('grid-box');
+            row.appendChild(gridBox);
+        }
+        container.appendChild(row);
+    }
+};
